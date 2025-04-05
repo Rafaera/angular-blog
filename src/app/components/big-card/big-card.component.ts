bc3e06d05a4f33ba3b5ccbb6a638e07c19edb3b0
Rafaela Santos <sant.rafaela08@gmail.com>
2025-04-05 12:37:06 -0300

    components criados com style

---
+++ src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  imports: [],
+  templateUrl: './big-card.component.html',
+  styleUrls: ['./big-card.component.css'],
+  
+})
+export class BigCardComponent {
+
+}
